aea9222170a4fee9abeb364ee73da2551dfd800c
omrajora <omrajora671@gmail.com>
2026-08-08 17:49:35 +0530

    fix gmail smtp config

--- routes/forgotPasswordRoutes.js
+++ routes/forgotPasswordRoutes.js
@@ -11,10 +11,15 @@ const router = express.Router();
 const resetTokens = {};
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
